4f8919703f2f4f201cb5cbc42f8d941c345a319a
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-03-12 00:14:10 +0900

    first commit

---
+++ src/main/java/com/kawakawaryuryu/samplespringbatch/SampleSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplespringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBatchApplication.class, args);
+	}
+
+}
