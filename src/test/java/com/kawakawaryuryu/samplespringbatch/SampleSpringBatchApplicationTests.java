d4118bad05e4bc22724b70a915f9b716da656bb0
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-03-17 22:30:21 +0900

    remove @SpringBootTest

--- src/test/java/com/kawakawaryuryu/samplespringbatch/SampleSpringBatchApplicationTests.java
+++ src/test/java/com/kawakawaryuryu/samplespringbatch/SampleSpringBatchApplicationTests.java
@@ -1,13 +1,29 @@
 package com.kawakawaryuryu.samplespringbatch;
 
+import com.kawakawaryuryu.samplespringbatch.config.BatchConfig;
+import com.kawakawaryuryu.samplespringbatch.config.WebConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.runner.RunWith;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBatchTest
+@EnableAutoConfiguration
+@ComponentScan
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        BatchConfig.class,
+        WebConfig.class
+})
 class SampleSpringBatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
