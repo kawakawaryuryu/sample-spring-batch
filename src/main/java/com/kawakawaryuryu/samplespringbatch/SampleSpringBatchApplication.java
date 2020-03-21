846df6f32c81e60f140678ee8737d0546b5e7467
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-03-21 21:42:39 +0900

    remove CommandLineRunner

--- src/main/java/com/kawakawaryuryu/samplespringbatch/SampleSpringBatchApplication.java
+++ src/main/java/com/kawakawaryuryu/samplespringbatch/SampleSpringBatchApplication.java
@@ -1,28 +1,12 @@
 package com.kawakawaryuryu.samplespringbatch;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleSpringBatchApplication implements CommandLineRunner {
-
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private Job job1;
+public class SampleSpringBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleSpringBatchApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        jobLauncher.run(job1, new JobParameters());
     }
 }
