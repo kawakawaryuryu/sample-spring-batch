9f18d51995583c880953d69cd2266e3623a0bb9e
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-03-22 02:15:47 +0900

    fix QiitaTasklet

--- src/main/java/com/kawakawaryuryu/samplespringbatch/step/chunk/qiita/QiitaArticle.java
+++ src/main/java/com/kawakawaryuryu/samplespringbatch/step/chunk/qiita/QiitaArticle.java
@@ -2,9 +2,11 @@ package com.kawakawaryuryu.samplespringbatch.step.chunk.qiita;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class QiitaArticle {
     private String id;
 
